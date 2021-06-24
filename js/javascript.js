749c1e6f7c8862eb432a035c9fa92a3f8ce22d69
Miguel Ponce Local <miguel_p1194@hotmail.com>
2021-06-23 19:39:33 -0500

    imagenes

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+// Pantalla de carga
+addEventListener('load',()=>{
+    const contenedor = document.querySelector('.contenedor');
+    contenedor.style.opacity=0;
+    contenedor.style.visibility= 'hidden';
+})
